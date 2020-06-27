3b167a2f98c1fae297ee7514567acc25dfc6a9ad
orsenkucher <orsen.kucher@gmail.com>
2020-06-27 20:36:19 +0300

    use GNOME Tweaks to tune keyboard instead

--- fish/config.fish
+++ fish/config.fish
@@ -1,13 +1,19 @@
+# Use `GNOME Tweaks` instead
+# Keyboard & Mouse -> 
+#					Overview Shortcut (Right Super)
+#					Additional Layout Options -> Ctrl position -> 
+#											Swap Ctrl and Caps Lock
+# 
 # make right super - left super
-xmodmap -e "keycode 134 = Super_L NoSymbol Super_L"
-
+#xmodmap -e "keycode 134 = Super_L NoSymbol Super_L"
+# 
 # swap casp with ctrl
-xmodmap -e "remove Lock = Caps_Lock"
-xmodmap -e "remove Control = Control_L"
-xmodmap -e "keysym Control_L = Caps_Lock"
-xmodmap -e "keysym Caps_Lock = Control_L"
-xmodmap -e "add Lock = Caps_Lock"
-xmodmap -e "add Control = Control_L"
+#xmodmap -e "remove Lock = Caps_Lock"
+#xmodmap -e "remove Control = Control_L"
+#xmodmap -e "keysym Control_L = Caps_Lock"
+#xmodmap -e "keysym Caps_Lock = Control_L"
+#xmodmap -e "add Lock = Caps_Lock"
+#xmodmap -e "add Control = Control_L"
 
 abbr -a .. 'cd ..'
 # abbr -a rm 'rm -i'
